705747d770e8d97cec757f5e867055771edbe77e
Eric Tang <tang.eric.h@gmail.com>
2025-08-19 01:34:30 -0400

    Renamed to BuddyLock from BuddyLockStarter

--- BuddyLock/ContentView.swift
+++ BuddyLock/ContentView.swift
@@ -66,7 +66,7 @@ struct ContentView: View {
                         .foregroundStyle(.secondary)
                 }
             }
-            .navigationTitle("BuddyLock Starter")
+            .navigationTitle("BuddyLock")
         }
     }
 }
