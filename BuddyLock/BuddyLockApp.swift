62d88caef35006eac9a06fd04453ac149545b8d4
1ssong2024 <1ssong2024@gmail.com>
2025-11-28 21:11:20 -0500

    firebase connected

--- BuddyLock/BuddyLockApp.swift
+++ BuddyLock/BuddyLockApp.swift
@@ -1,9 +1,20 @@
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct BuddyLockApp: App {
     @StateObject private var screenTime = ScreenTimeManager()
-
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             MainTabView()            // 👈 NEW ROOT VIEW
